2a521c6a3e4563b1f07d3799fa50e17c05bab274
Natalia Schurbert <jaleika84@gmail.com>
2023-05-25 17:22:37 +0200

    Changing face detection with cv to face detection with YOLO5

--- face_detection/main.py
+++ face_detection/main.py
@@ -1,18 +1,22 @@
+from yolo5face.get_model import get_model
 from pathlib import Path
 import cv2
-import cvlib as cv
 
 
-def crop_faces(image_path: str = "face_detection/test_image_5.jpg"):
+def crop_faces(image_path: str = "face_detection/test_image_1.jpg"):
 
     """Extracts faces from an image and save them as separate images in "export" folder
     Args: image path
     """
+    # Load the model
+    model = get_model("yolov5n", gpu=-1, target_size=512, min_face=24)
     # Load an iamge from the path
     image = cv2.imread(image_path)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     # Perform face detection
-    faces, _confidences = cv.detect_face(image)
-    for index, face in enumerate(faces):
+    faces, _key_points = model(image)
+    for index, face in enumerate(faces[0]):
         cropped_image = image[
             face[1] : face[3],
             face[0] : face[2],
