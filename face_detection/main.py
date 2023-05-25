cf71290b1234e2343ef93de0625a209c74215e73
Natalia Schurbert <jaleika84@gmail.com>
2023-05-25 15:25:28 +0200

    Created a function crop_faces, which extracts faces from an image and save them as separate images in export folder

--- face_detection/main.py
+++ face_detection/main.py
@@ -1,38 +1,14 @@
 from pathlib import Path
-
 import cv2
 import cvlib as cv
 
-# from matplotlib import pyplot as plt
-# from cvlib.object_detection import detect_common_objects, draw_bbox
 
-# Load and process images
-# image_paths = [
-#     "face_detection/test_image_1.jpg",
-#     "face_detection/test_image_2.jpg",
-#     "face_detection/test_image_3.jpg",
-# ]
-# images = [cv2.imread(image_path) for image_path in image_paths]
+def crop_faces(image_path: str = "face_detection/test_image_5.jpg"):
 
-# # Plot the images with bounding boxes
-# fig, axs = plt.subplots(1, len(images), figsize=(12, 6))
-# for i, image in enumerate(images):
-#     # Perform face detection
-#     faces, confidences = cv.detect_face(image)
-
-#     # Draw bounding boxes around the detected faces (if any)
-#     for (x, y, w, h), confidence in zip(faces, confidences):
-#         cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
-
-#     axs[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-#     axs[i].set_title(f"Image {i+1}")
-#     axs[i].axis("off")
-
-# plt.tight_layout()
-# plt.show()
-
-
-def crop_faces(image_path: str = "face_detection/test_image_1.jpg"):
+    """Extracts faces from an image and save them as separate images in "export" folder
+    Args: image path
+    """
+    # Load an iamge from the path
     image = cv2.imread(image_path)
     # Perform face detection
     faces, _confidences = cv.detect_face(image)
