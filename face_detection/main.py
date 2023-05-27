3c13b605164fe30236ab0dd4eec5ddb52b49d05e
Natalia Schurbert <jaleika84@gmail.com>
2023-05-27 17:22:04 +0200

    Implemented histogram_equalization function to the main.py in face_detection

--- face_detection/main.py
+++ face_detection/main.py
@@ -3,16 +3,28 @@ from pathlib import Path
 import cv2
 
 
-def crop_faces(image_path: str = "face_detection/test_image_3.jpg"):
+def histogram_equalization(image):
+
+    """Applies histogram equalisation to the original image"""
+
+    # Split the image into its color channels
+    b, g, r = cv2.split(image)
+    # Equalize the histogram for each color channel
+    b_eq = cv2.equalizeHist(b)
+    g_eq = cv2.equalizeHist(g)
+    r_eq = cv2.equalizeHist(r)
+    # Merge the equalized channels back together
+    return cv2.merge([b_eq, g_eq, r_eq])
+
+
+def crop_faces(image, image_path):
 
     """Extracts faces from an image and save them as separate images in "export" folder
     Args: image path
     """
+
     # Load the model
     model = get_model("yolov5n", gpu=-1, target_size=512, min_face=24)
-    # Load an iamge from the path
-    image = cv2.imread(image_path)
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Perform face detection
     faces, _key_points = model(image)
@@ -29,5 +41,17 @@ def crop_faces(image_path: str = "face_detection/test_image_3.jpg"):
         )
 
 
+def main(image_path: str = "face_detection/test_image_1.jpg", should_equalize=True):
+
+    """Reads the image from the path, then performs histogram equalization and extracts the faces"""
+
+    image = cv2.imread(image_path)
+    if should_equalize:
+        equalized_image = histogram_equalization(image)
+        crop_faces(equalized_image, image_path)
+    else:
+        crop_faces(image, image_path)
+
+
 if __name__ == "__main__":
-    crop_faces()
+    main()
--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,10 +1,10 @@
 import cv2
-from matplotlib import pyplot as plt
 from pathlib import Path
 
 # Read the image in color mode
-image_path = "face_detection/test_image_3.jpg"
+image_path = "face_detection/test_image_2.jpg"
 img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
 # Split the image into its color channels
 b, g, r = cv2.split(img)
 # Equalize the histogram for each color channel
@@ -16,5 +16,5 @@ img2 = cv2.merge([b_eq, g_eq, r_eq])
 # Save the equalized image
 path = Path(image_path)
 cv2.imwrite(f"preprocessing/export/{path.name}", img2)
-# Convert color for matplotlib (OpenCV uses BGR while matplotlib uses RGB)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+# # Convert color for matplotlib (OpenCV uses BGR while matplotlib uses RGB)
+# img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
