cda4ab4afa3eabdcb615ebbf7ba0a7123878b991
Isabella Retter <isabella.retter@posteo.de>
2023-06-03 17:03:01 +0200

    models changed to compiled models on disk, please download latest_model.h5 from google drive link I provide

--- decode/api/fast.py
+++ decode/api/fast.py
@@ -13,30 +13,37 @@ import pickle
 
 app = FastAPI()
 # the way to load the model into memory
-model_face_detection = get_face_detection_model()
-DIR_MODELS = f"{LOCAL_MODELS_DATA_PATH}/models"
-model_emotion = load_model(f'{DIR_MODELS}/Trained_InceptionResNetV2_resize_3')
+#app.state.model_face_detection = get_face_detection_model()
 
+DIR_MODELS = f"{LOCAL_MODELS_DATA_PATH}/models"
+
+# put the latest model into the models-folder and rename it to 'latest_model'
+app.state.model_emotion = load_model(f'{DIR_MODELS}/latest_model.h5')
+with open(f'{DIR_MODELS}/model_face_detection.pkl', 'rb') as f:
+    app.state.model_face_detection = pickle.load(f)
+#app.state.model_face_detection = get_face_detection_model()
 
 @app.post("/predict")
 async def predict(image: UploadFile=File()):
     content = await image.read()
     nparr =  np.fromstring(content, np.uint8)
     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
-    with open("cv2_img.pkl", "wb") as file: pickle.dump(cv2_img, file)
+    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     eq_img = histogram_equalization(cv2_img)
-    with open("eq_img.pkl", "wb") as file: pickle.dump(eq_img, file)
-
-    corners, key_points = model_face_detection.predict(eq_img)
+    corners, key_points = app.state.model_face_detection.predict(eq_img)
     y_pred = []
-    for face_corners in corners[0]:
+    for i in range(len(corners[0])):
+        face_corners = corners[0][i]
         face_img = eq_img[
                     face_corners[1] : face_corners[3],
                     face_corners[0] : face_corners[2],
                 ]
-        face_img_resize = tf.image.resize(face_img, [150,150])
-        y_pred.append(model_emotion.predict(np.expand_dims(face_img_resize,0)))
-    print(f"y_pred is {y_pred}")
+
+        face_bw = tf.stack([face_img.mean(axis = -1), face_img.mean(axis = -1), face_img.mean(axis = -1)], axis =  -1)
+        face_bw_resize = tf.image.resize(face_bw, [150,150])
+        y_pred.append(app.state.model_emotion .predict(np.expand_dims(face_bw_resize,0)))
+#        y_pred.append(app.state.model_emotion.predict(np.expand_dims(face_img_resize,0)))
+
     detection =       {0: 'angry',
                         1: 'disgust',
                         2: 'fear',
